e1689cb60a0a271511b7cb832ba54785bfac9401
dradkov <dimitar_radkov@abv.bg>
2017-10-22 21:53:42 +0300

    Add functionality

--- scripts/prodavachnik.js
+++ scripts/prodavachnik.js
@@ -1,4 +1,11 @@
 function startApp() {
+	
+	    if (sessionStorage.getItem('authToken') !== null) {
+        let username = sessionStorage.getItem('username');
+        $('#loggedInUser').text("Welcome, " + username + "!");
+    }
+
+	
     showHideMenuLinks();
     showHomeView();
 
@@ -8,6 +15,36 @@ function startApp() {
     $("#linkRegister").click(showRegisterView);
 	$("#linkListAds").click(listAdverts);
     $("#linkLogout").click(logoutUser);
+	
+	    function showHideMenuLinks() {
+        $("#linkHome").show();
+        if (sessionStorage.getItem('authToken') === null) {
+            // No logged in user
+            $("#linkLogin").show();
+            $("#linkRegister").show();
+            $("#linkListAds").hide();
+            $("#linkLogout").hide();
+            $("#loggedInUser").hide();
+        } else {
+            // We have logged in user
+            $("#linkLogin").hide();
+            $("#linkRegister").hide();
+            $("#linkListAds").show();
+            $("#linkLogout").show();
+            $("#loggedInUser").show();
+        }
+    }
+
+	    function saveAuthInSession(userInfo) {
+        let userAuth = userInfo._kmd.authtoken;
+        sessionStorage.setItem('authToken', userAuth);
+        let userId = userInfo._id;
+        sessionStorage.setItem('userId', userId);
+        let username = userInfo.username;
+        sessionStorage.setItem('username', username);
+        $('#loggedInUser').text("Welcome, " + username + "!");
+    }
+
 
     // Bind the form submit buttons
     $("#buttonLoginUser").click(loginUser);
